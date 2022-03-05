2798626f0feeac64a48cbaa0c36efc10ea150ee5
coco <kokotenrico@gmail.com>
2022-03-05 14:24:19 +0100

    Restyle HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="jumbotron">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-4"><img alt="Vue logo" src="../assets/logo.png" /></h1>
+      <p class="lead">
+        <HelloWorld msg="Welcome to Your Vue.js App" />
+      </p>
+    </div>
   </div>
 </template>
 
