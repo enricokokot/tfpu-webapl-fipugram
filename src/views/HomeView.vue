ece59d1c41ee4dd47fdc845842212ffd9d09c452
coco <kokotenrico@gmail.com>
2022-03-06 08:46:41 +0100

    Add Dummy Post

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,16 @@
 <template>
-  <div class="jumbotron jumbotron-fluid">
-    <div class="container">
-      <h1 class="display-4"><img alt="Vue logo" src="../assets/logo.png" /></h1>
-      <p class="lead">
-        <HelloWorld msg="Welcome to Your Vue.js App" />
-      </p>
+  <div class="row">
+    <div class="col-2"></div>
+    <div class="col-7">
+      <div class="card text-center">
+        <div class="card-header">Featured</div>
+        <div class="card-body p-0">
+          <img class="card-img-top" src="https://picsum.photos/800" />
+        </div>
+        <div class="card-footer text-muted">2 days ago</div>
+      </div>
     </div>
+    <div class="col-3">... ovdje ide desni stupac!</div>
   </div>
 </template>
 
