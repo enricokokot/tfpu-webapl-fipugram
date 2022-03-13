36277c7583fb3f69b44ae0b154930af14d95abeb
coco <kokotenrico@gmail.com>
2022-03-13 11:52:17 +0100

    Refactor Signup, Implement Firebase Signup Auth

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAt1a9FGGRLYztpmOj76LVmJsl_-a6ogMs",
+  authDomain: "fipugram-4bd0c.firebaseapp.com",
+  projectId: "fipugram-4bd0c",
+  storageBucket: "fipugram-4bd0c.appspot.com",
+  messagingSenderId: "1075526174075",
+  appId: "1:1075526174075:web:8e9c559e76e36e630429de",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -7,27 +7,10 @@
         <div class="col-sm">
           <form>
             <div class="form-group">
-              <label for="exampleFirstName1">First Name</label>
-              <input
-                type="text"
-                class="form-control"
-                id="exampleFirstName1"
-                placeholder="Enter first name"
-              />
-            </div>
-            <div class="form-group">
-              <label for="exampleLastName1">Last Name</label>
-              <input
-                type="text"
-                class="form-control"
-                id="exampleLastName1"
-                placeholder="Enter last name"
-              />
-            </div>
-            <div class="form-group">
               <label for="exampleInputEmail1">Email address</label>
               <input
                 type="email"
+                v-model="username"
                 class="form-control"
                 id="exampleInputEmail1"
                 aria-describedby="emailHelp"
@@ -41,12 +24,25 @@
               <label for="exampleInputPassword1">Password</label>
               <input
                 type="password"
+                v-model="password"
                 class="form-control"
                 id="exampleInputPassword1"
                 placeholder="Password"
               />
             </div>
-            <button type="submit" class="btn btn-primary">Sign Up</button>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Repeat Password</label>
+              <input
+                type="password"
+                v-model="passwordRepeat"
+                class="form-control"
+                id="exampleInputPassword2"
+                placeholder="Password"
+              />
+            </div>
+            <button type="button" @click="signup" class="btn btn-primary">
+              Sign Up
+            </button>
           </form>
         </div>
         <div class="col-sm"></div>
@@ -54,3 +50,27 @@
     </div>
   </div>
 </template>
+
+<script>
+// import app from "@/firebase";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+// import { app } from "@/firebase";
+
+export default {
+  name: "Signup",
+  data() {
+    return { username: "", password: "", passwordRepeat: "" };
+  },
+  methods: {
+    signup() {
+      const auth = getAuth();
+      createUserWithEmailAndPassword(auth, this.username, this.password)
+        .then(() => console.log("Uspješna registracija"))
+        .catch((error) =>
+          alert("Odabrana šifra je prekratka, molim vas odaberite neku drugu")
+        );
+      console.log("Nastavak");
+    },
+  },
+};
+</script>
