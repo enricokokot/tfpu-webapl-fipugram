7a55a91bb27e6c2e377e8497e8dc2787b5c5e8df
coco <kokotenrico@gmail.com>
2022-03-13 08:53:06 +0100

    Refactor InstagramCard to Receive More Info

--- src/components/InstagramCard.vue
+++ src/components/InstagramCard.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="card text-center mb-3">
-    <div class="card-header">Featured</div>
+    <div class="card-header">{{ card.description }}</div>
     <div class="card-body p-0">
-      <img class="card-img-top" :src="photo" />
+      <img class="card-img-top" :src="card.url" />
     </div>
     <PostButtons />
-    <div class="card-footer text-muted">2 days ago</div>
+    <div class="card-footer text-muted">{{ card.time }}</div>
   </div>
 </template>
 
@@ -13,7 +13,7 @@
 import PostButtons from "@/components/PostButtons.vue";
 
 export default {
-  props: ["photo"],
+  props: ["card"],
   name: "InstagramCard",
   components: {
     PostButtons,
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <div class="row">
     <div class="col-2"></div>
     <div class="col-7">
-      <instagram-card v-for="card in cards" :key="card" :photo="card" />
+      <instagram-card v-for="card in cards" :key="card.url" :card="card" />
     </div>
     <div class="col-3"></div>
   </div>
@@ -13,9 +13,21 @@
 import InstagramCard from "@/components/InstagramCard.vue";
 
 const cards = [
-  "https://picsum.photos/id/1/800",
-  "https://picsum.photos/id/2/800",
-  "https://picsum.photos/id/3/800",
+  {
+    url: "https://picsum.photos/id/1/800",
+    description: "laptop #1",
+    time: "few minutes ago...",
+  },
+  {
+    url: "https://picsum.photos/id/2/800",
+    description: "laptop #2",
+    time: "hour ago...",
+  },
+  {
+    url: "https://picsum.photos/id/3/800",
+    description: "laptop #3",
+    time: "few hours ago...",
+  },
 ];
 
 export default {
